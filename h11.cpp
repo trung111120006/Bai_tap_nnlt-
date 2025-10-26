30f5c5a5f9f3438ebfceb4258e36fb366502a9f6
trung111120006 <trunglq112006@gmail.com>
2025-10-27 01:27:08 +0700

    Làm sau

--- h11.cpp
+++ h11.cpp
@@ -1,25 +1,12 @@
-//Bài tập tính det ma trận vuông bất kỳ 
 //bài tập tính phần tử nhỏ nhất trong ma trận đa chiều 
 #include <bits/stdc++.h>
 using namespace std;
 
-void Min_number_maxtri(int maxtrix[], int n) {
-    int temp = maxtrix[0];
-    for (int i = 1; i < n; i++) {
-        if (maxtrix[i] < temp) {
-            temp = maxtrix[i];
-        }
-    }
-    cout << temp << endl;
+void Min_number_maxtri() {
+  
 }
 
 int main() {
-    int n;
-    cin >> n;
-    int maxtrix[n];
-    for (int i = 0; i < n; i++) {
-        cin >> maxtrix[i];
-    }
-    Min_number_maxtri(maxtrix, n);
+   
     return 0;
 }
