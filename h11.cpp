80b11c97db1ea72d873755c0b7d946aad67dfc25
trung111120006 <trunglq112006@gmail.com>
2025-10-27 22:48:38 +0700

    H11 success

--- h11.cpp
+++ h11.cpp
@@ -2,21 +2,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void Min_number_maxtri() {
-  
-}
-
 int main() {
     int n;
-    cout << "Enter the number of dimensions: ";
     cin >> n;
-    vector <int> matrix ;
-    while (n >0){
-        int x;
-        cin >> x;
-        matrix.push_back(x);
-        n--;
 
+    vector<int> kichthuoc(n);
+    long long total_size = 1;
+
+    // Nhập kích thước từng chiều và tính tổng số phần tử
+    for (int i = 0; i < n; i++) {
+        cin >> kichthuoc[i];
+        total_size *= kichthuoc[i];
     }
+
+    vector<int> a(total_size);
+    int min_val, max_val;
+
+    for (long long i = 0; i < total_size; i++) {
+        cin >> a[i];
+        if (i == 0) {
+            min_val = max_val = a[i];
+        } else {
+            if (a[i] < min_val) min_val = a[i];
+            if (a[i] > max_val) max_val = a[i];
+        }
+    }
+
+    cout << "Min = " << min_val << endl;
+    cout << "Max = " << max_val << endl;
+
     return 0;
 }
