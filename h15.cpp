22d1ab01c9a73b2bfc54e7d24596835d25a7e861
trung111120006 <trunglq112006@gmail.com>
2025-10-28 09:06:38 +0700

    .

--- h15.cpp
+++ h15.cpp
@@ -1,3 +1,29 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+struct day
+{
+    int date;
+    int month;
+    int year;
+};
+struct tomorrow
+{
+    day today;
+    int days_after;
+};
+int main (){
+    tomorrow t;
+    cout << "Enter day, month, year: ";
+    cin >> t.today.date >> t.today.month >> t.today.year;
+    cout << "Enter number of days to add: ";
+    cin >> t.days_after;
+
+    // Simple addition without considering month/year overflow
+    t.today.date += t.days_after;
+
+    cout << "New date after adding " << t.days_after << " days is: "
+         << t.today.date << "/" << t.today.month << "/" << t.today.year << endl;
+
+    return 0;
+}
